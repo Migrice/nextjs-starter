616c0f35f17bb9aa253e61a944e7a648cf67df87
Migrice <efomenakuete@gmail.com>
2025-03-27 16:13:39 +0100

    initial setup

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className="py-6">
+      <div className="container">
+        <h1 className="">Next JS starter kit</h1>
+      </div>
+    </section>
+  );
+}

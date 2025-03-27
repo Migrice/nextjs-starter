9e0efa665d5f5ff44e9fc1530349745fc143ac4c
Migrice <efomenakuete@gmail.com>
2025-03-27 16:42:00 +0100

    modify style

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
-    <section className="py-6">
-      <div className="container">
-        <h1 className="">Next JS starter kit</h1>
+    <section>
+      <div className="w-full">
+        <h1 className="mx-2 bg-gray-800 py-4 text-center text-2xl font-bold text-white">
+          Next JS starter kit
+        </h1>
       </div>
     </section>
   );
